6295cba86a361f745b013e3f9d2cfe5f09f00a84
Martin Lyrå <martin.lyra.97@gmail.com>
2016-10-26 14:39:41 +0200

    Assignment 1.9 "Tiden.class"

--- ml224ec_lab1/Tiden.java
+++ ml224ec_lab1/Tiden.java
@@ -4,23 +4,32 @@ import java.util.Scanner;
 
 public class Tiden {
 	
-	private static final int HoursInMinutes = 60;
-	private static final int MinutesInSeconds = 60;
+	private static final int HourInMinutes = 60;
+	private static final int MinuteInSeconds = 60;
 	
-	private static final int HoursInSeconds = HoursInMinutes * MinutesInSeconds;
+	private static final int HourInSeconds = HourInMinutes * MinuteInSeconds;
 
 	public static void main(String[] args) {
 		
-		int hours;
-		int minutes;
-		int seconds;
+		int totalHours;
+		int totalMinutes;
+		int totalSeconds;
 		
+		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Ange antal sekunder: ");
+		int seconds = input.nextInt();
 		
+		totalHours = (int)Math.floor(seconds/HourInSeconds);
+		seconds -= totalHours*HourInSeconds;
 		
-		System.out.print("Detta motsvarar %d timmar, %d minuter och %d sekunder.");
+		totalMinutes = (int)Math.floor(seconds/MinuteInSeconds);
+		seconds -= totalMinutes*MinuteInSeconds;
+		
+		totalSeconds = seconds;
+		
+		System.out.printf("Detta motsvarar %d timmar, %d minuter och %d sekunder.\n",totalHours, totalMinutes, totalSeconds);
 
 	}
 
