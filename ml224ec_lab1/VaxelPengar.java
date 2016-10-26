d0de0a4b0d89b2728a16af159ed801ac087c30fb
Martin Lyrå <martin.lyra.97@gmail.com>
2016-10-26 16:33:00 +0200

    Assignment 1.11 "VaxelPengar.java"

---
+++ ml224ec_lab1/VaxelPengar.java
@@ -0,0 +1,69 @@
+package ml224ec_lab1;
+
+import java.util.Scanner;
+
+public class VaxelPengar {
+	
+	private static class CurrencyInfo {
+		public final int Value;
+		public final CurrencyType Type;
+		
+		public CurrencyInfo(int value, CurrencyType type)
+		{
+			this.Value = value;
+			this.Type = type;
+		}
+		
+		public String toString()
+		{
+			return Integer.toString(Value) + "-" + (Type == CurrencyType.BILL ? "lappar" : "kronor");
+		}
+	}
+	
+	private enum CurrencyType {
+		BILL,
+		COIN
+	}
+	
+	private static CurrencyInfo[] currencyInfos = 
+		{
+			new CurrencyInfo( 1000, CurrencyType.BILL ),
+			new CurrencyInfo( 500, 	CurrencyType.BILL ),
+			new CurrencyInfo( 100, 	CurrencyType.BILL ),
+			new CurrencyInfo( 50, 	CurrencyType.BILL ),
+			new CurrencyInfo( 20, 	CurrencyType.BILL ),
+			new CurrencyInfo( 10, 	CurrencyType.COIN ),
+			new CurrencyInfo( 5, 	CurrencyType.COIN ),
+			new CurrencyInfo( 1, 	CurrencyType.COIN )
+		};
+
+	public static void main(String[] args) {
+		
+		double cost;
+		int payment;
+		int change;
+		
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Ange kostnaden: ");
+		cost = input.nextDouble();
+		
+		System.out.print("Ange erhållet belopp: ");
+		payment = input.nextInt();
+		
+		change = (int) Math.round(payment - cost);
+		System.out.printf("Växel tillbaka: %d\n", change);
+		
+		int changeRemainder = change;
+		for (int i = 0; i < currencyInfos.length; i++)
+		{
+			int value = currencyInfos[i].Value;
+			int count = (int) Math.floor(changeRemainder/value);
+			System.out.printf("%s\t: %d\n", currencyInfos[i], count);
+			changeRemainder -= value*count;
+		}	
+		
+	}
+
+}
