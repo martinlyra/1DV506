226803dd20896bf797ea7ab18ae3aff5a527e803
reversed_word <martin.lyra.97@gmail.com>
2016-11-08 11:26:50 +0100

    Assignment 2.5 Triangle.java

---
+++ ml224ec_lab2/Triangle.java
@@ -0,0 +1,53 @@
+package ml224ec_lab2;
+
+import java.util.*;
+
+public class Triangle {
+
+	private static final Scanner INPUT = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		System.out.print("Ange ett positivt udda tal: ");
+		int val = INPUT.nextInt();
+		if ((val % 2) == 0 || val < 0) // if "val" is an even number or a number smaller than 0 (is a negative number)
+		{
+			System.out.println("Programfel: Förväntade ett positivt udda tal, fick annat.");
+			return;
+		}
+		
+		System.out.println("Rätvinklig triangel:");
+		String triangle_a = "";
+		for (int i = 1; i <= val; i++)
+		{
+			String buff = "";
+			for (int I = 0; I < i; I++)
+				buff += '*';
+			triangle_a += buff + '\n';
+		}
+		System.out.print(triangle_a);
+		
+		System.out.println("Likbent triangel: ");
+		String triangle_b = "";
+		
+		/* This is how you, as a normal sane mathematican, would do
+		* int base = val/2;
+		* int height = (int)Math.sqrt( (Math.pow(val, 2) - Math.pow(base, 2)) );
+		*/ // But goddamn Java and all those text-only outputs
+		
+		int height = (val - 1)/2 + 1; // Now with 100% text support!
+		
+		for (int i = 0; i < height; i++)
+		{
+			String buff = "";
+			for (int I = 0; I < val/2 - i; I++) // shrink with one each side, starting without one in middle
+				buff += ' ';
+			for (int I = 0; I < i*2 + 1; I++)	// grow with one on each side, starting with one in middle
+				buff += '*';
+			triangle_b += buff + '\n';
+		}
+		System.out.print(triangle_b);
+
+	}
+
+}
