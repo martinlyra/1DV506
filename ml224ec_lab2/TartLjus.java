8bd77fd76df7314bf41632b877e3a8a0fd394b23
reversed_word <martin.lyra.97@gmail.com>
2016-11-15 11:41:14 +0100

    Assignment 2.8 TartLjus.java

---
+++ ml224ec_lab2/TartLjus.java
@@ -0,0 +1,47 @@
+package ml224ec_lab2;
+
+public class TartLjus {
+
+	private static final int CANDLES_PER_BOX = 24;
+	private static final int MAX_AGE = 100;
+	
+	public static void main(String[] args) {
+		
+		int remaining = 0;
+		int boxes = 0;
+		
+		for (int i = 0;i < MAX_AGE; i++)
+		{
+			int age = i+1;
+			int bought = 0;
+			
+			remaining -= age;
+			
+			while (remaining < 0)
+			{
+				remaining += CANDLES_PER_BOX;
+				bought++;
+			}
+			
+			System.out.printf(
+					"Till din %d:%s födelsedag, köp "
+					+ (bought > 0 ? "%d låd%s" : "inget")
+					+ ".\n"
+					, age
+					, (i < 3 ? "a" : "e")
+					, bought
+					, (bought > 1 ? "or" : "a")
+					);
+			boxes += bought;
+		}
+
+		System.out.printf(
+				"Under dina %d år har %d låd%s av ljus köpts. Kvar över blev %s ljus."
+				, MAX_AGE
+				, boxes
+				, (boxes > 1 ? "or" : "a")
+				, (remaining > 0 ? remaining : "inga")
+				);
+	}
+
+}
