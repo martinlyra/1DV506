2eb04067a1c35301dc8b8426ff0a3a89db582f53
reversed_word <martin.lyra.97@gmail.com>
2016-11-15 11:36:14 +0100

    Assignment 2.11 OmvandOrdning.java

---
+++ ml224ec_lab2/OmvandOrdning.java
@@ -0,0 +1,30 @@
+package ml224ec_lab2;
+
+import java.util.*;
+
+public class OmvandOrdning {
+	
+	private static final ArrayList<Integer> INTEGERS = new ArrayList<Integer>();
+	private static final Scanner INPUT = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		int count = 0;
+		int input = 0;
+		
+		System.out.println("Mata in positiva tal; avsluta med negativt tal.");
+		while (input > -1)
+		{
+			System.out.printf("Tal %d: ", count++);
+			input = INPUT.nextInt();
+			if (input < 0)
+				break;
+			INTEGERS.add(input);
+		}
+
+		System.out.printf("Antal positiva tal: %d\nBaklänges: ", INTEGERS.size());
+		for (int i = INTEGERS.size()-1; i >= 0; i--)
+			System.out.printf("%d ", INTEGERS.get(i));
+	}
+
+}
