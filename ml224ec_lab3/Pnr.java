c61a9d1af96ae4569e4b4413d6e8d039277c2da8
reversed_word <martin.lyra.97@gmail.com>
2016-12-08 20:36:46 +0100

    Extended solution for 3.2 (Pnr.java)

--- ml224ec_lab3/Pnr.java
+++ ml224ec_lab3/Pnr.java
@@ -3,42 +3,51 @@ package ml224ec_lab3;
 public class Pnr {
 
 	private static final String NUMBER_FORMAT = "YYMMDD-NNNN";
-	//private static final String NUMBER_FORMAT_LONG = "YYYYMMDD-NNNN";
 	
 	private static final int FIRST_PART_LENGTH = 6;
-	//private static final int FIRST_PART_LENGTH_LONG = FIRST_PART_LENGTH + 2; // 8
 	private static final int SECOND_PART_LENGTH = 4;
 	
 	private static final int GENDER_DIGIT_OFFSET = 2; // starting from 0
 	
-	// private static String PERSON_NUMBER = "";
-	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// PERSON_NUMBER = NUMBER_FORMAT;
 		
-		String pnr1 = "970511-2425";
+		String pnr1 = "970511-2424";
 		String pnr2 = "781201-1139";
-		String pnr3 = "880824-3190";
+		String pnr3 = "890823-3193";
 		
+		System.out.println("functionName():");
+		System.out.printf("pnr1 (%s)\n",pnr1);
+		System.out.printf("pnr2 (%s)\n",pnr2);
+		System.out.printf("pnr3 (%s)\n",pnr3);
+		
+		System.out.println("\ngetFirstPart():");
 		System.out.println(getFirstPart(pnr1));
 		System.out.println(getFirstPart(pnr2));
 		System.out.println(getFirstPart(pnr3));
 		
+		System.out.println("\ngetSecondPart():");
 		System.out.println(getSecondPart(pnr1));
 		System.out.println(getSecondPart(pnr2));
 		System.out.println(getSecondPart(pnr3));
 		
+		System.out.println("\nisMale():");
 		System.out.println(isMale(pnr1));
 		System.out.println(isMale(pnr2));
 		System.out.println(isMale(pnr3));
 		
+		System.out.println("\nisFemale():");
 		System.out.println(isFemale(pnr1));
 		System.out.println(isFemale(pnr2));
 		System.out.println(isFemale(pnr3));
 		
-		System.out.println(areEqual(pnr1, pnr1));
-		System.out.println(areEqual(pnr2, pnr3));
+		System.out.println("\nareEqual():");
+		System.out.printf("pnr1, pnr1: %s\n", areEqual(pnr1, pnr1));
+		System.out.printf("pnr2, pnr3: %s\n", areEqual(pnr2, pnr3));
+		
+		System.out.println("\nisCorrect():");
+		System.out.println(isCorrect(pnr1));
+		System.out.println(isCorrect(pnr2));
+		System.out.println(isCorrect(pnr3));
 	}
 	
 	public static String getFirstPart(String pnr)
@@ -76,4 +85,26 @@ public class Pnr {
 		return n%2==0;
 	}
 	
+	// see the Luhn algorithm for this
+	// Basically, you take each individual digit then multiply it
+	// with the factor (2 - n%2), starting with 0, it gives you 2, 1, 2, 1
+	// Then you add every digit present in the product number
+	// (with (n - n%10)/10 + n%10, for two-digit numbers) into a single sum
+	private static boolean isCorrect(String pnr)
+	{
+		String clean = pnr.replaceAll("-", ""); // clean out the non-digits (lazily)
+		int sum = 0;
+		
+		for (int i = 0; i < clean.length(); i++)
+		{
+			int literal = Integer.parseInt(clean.substring(i, i+1));
+			int factor = 2 - i%2; // 0%2 = 0, 1%2 = 1, 2%2 = 0 -- starting with 2
+			int product = literal*factor;
+			
+			int remainder = product%10;
+			sum += (product - remainder)/10 + remainder;
+		}
+		
+		return (sum%10) == 0;
+	}
 }
